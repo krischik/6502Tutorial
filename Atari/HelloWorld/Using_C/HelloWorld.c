29f8ab0119b422a7dce6bf4fbcb74bd4d380fd20
Martin Krischik <krischik@users.sourceforge.net>
2020-10-05 11:27:27 +0200

    Enhancement #1 Initial checkin.

---
+++ Atari/HelloWorld/Using_C/HelloWorld.c
@@ -0,0 +1,12 @@
+
+#include <atari.h>
+#include <stdio.h>
+
+void main()
+{
+    puts ("Hello World!");
+    return;
+}
+
+/* vim: set nowrap tabstop=8 shiftwidth=4 softtabstop=4 noexpandtab : */
+/* vim: set textwidth=0 filetype=c foldmethod=marker nospell : */
