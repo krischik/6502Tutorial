8961f00ae62869b0803b645106d1dbcb5740981c
Martin Krischik <krischik@users.sourceforge.net>
2020-11-16 20:06:57 +0100

    Issue #9 : LED rotator.

---
+++ Kit/Rotate_LED/src/main/asm/Rotate_LED.s
@@ -0,0 +1,54 @@
+;############################################################ {{{1 ##########
+;  Copyright © 2020 … 2020 Martin Krischik «krischik@users.sourceforge.net»
+;############################################################################
+;  This program is free software: you can redistribute it and/or modify
+;  it under the terms of the GNU General Public License as published by
+;  the Free Software Foundation, either version 3 of the License, or
+;  (at your option) any later version.
+;
+;  This program is distributed in the hope that it will be useful,
+;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;  GNU General Public License for more details.
+;
+;  You should have received a copy of the GNU General Public License
+;  along with this program.  If not, see «http://www.gnu.org/licenses/».
+;############################################################ }}}1 ##########
+
+ORB	    =	    $7F00
+DDRB	    =	    $7F02
+
+NMIB	    =	    $FFFA
+RESB	    =	    $FFFC
+IRQB	    =	    $FFFE
+
+.macro	    Set_B   value
+	    LDA	    value
+	    STA	    DDRB
+.endmacro
+
+.macro	    Out_B   value
+	    LDA	    value
+	    STA	    ORB
+.endmacro
+
+.segment    "CODE"
+
+DO_RES:	    Set_B   #$FF
+	    Out_B   #$50
+	    
+loop:	    ROR
+	    BRA	    loop
+
+DO_NMI:	    RTI
+
+DO_IRQ:	    RTI
+
+.segment    "HEADER"
+.word	    DO_NMI
+.word	    DO_RES
+.word	    DO_IRQ
+
+;############################################################ {{{1 ##########
+; vim: set nowrap tabstop=8 shiftwidth=4 softtabstop=4 noexpandtab :
+; vim: set textwidth=0 filetype=asm foldmethod=marker nospell :
